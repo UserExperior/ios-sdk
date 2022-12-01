f5a9c2cb29bcee5420526392bedb9b5fe2ac1c30
Shekhar Gupta <shekhar@userexperior.com>
2022-12-01 15:55:07 +0530

    Bug fixes and version update

---
+++ UserExperiorSDK.xcframework/ios-arm64/UserExperiorSDK.framework/PrivateHeaders/UserExperiorPrivateHeaders.h
@@ -0,0 +1,13 @@
+//
+//  UserExperiorPrivateHeaders.h>
+//  UserExperiorSDK
+//
+//  Created by Arkadi Yoskovitz on 3/6/21.
+//
+
+#ifndef PrivateHeaders_h
+#define PrivateHeaders_h
+
+#import <UserExperiorSDK/UEZipArchive.h>
+
+#endif /* PrivateHeaders_h */
