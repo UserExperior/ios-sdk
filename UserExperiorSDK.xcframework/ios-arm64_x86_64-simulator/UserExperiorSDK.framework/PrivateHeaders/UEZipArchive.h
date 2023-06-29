c6e365583ba812d969375ba3806dee163bf8edcb
Shekhar Gupta <shekhar@userexperior.com>
2023-06-29 12:42:16 +0530

    Updating SDK for v6.0.0

---
+++ UserExperiorSDK.xcframework/ios-arm64_x86_64-simulator/UserExperiorSDK.framework/PrivateHeaders/UEZipArchive.h
@@ -0,0 +1,33 @@
+//
+//  UEZipArchive.h
+//  UserExperiorSDK
+//
+//  Created by Arkadi Yoskovitz on 3/6/21.
+//
+
+#ifndef UEZipArchive_h
+#define UEZipArchive_h
+
+#import <UserExperiorSDK/SSZipArchive.h>
+#import <UserExperiorSDK/SSZipCommon.h>
+
+#import <UserExperiorSDK/aes.h>
+#import <UserExperiorSDK/aes_ni.h>
+#import <UserExperiorSDK/aesopt.h>
+#import <UserExperiorSDK/aestab.h>
+#import <UserExperiorSDK/brg_endian.h>
+#import <UserExperiorSDK/brg_types.h>
+#import <UserExperiorSDK/crypt.h>
+#import <UserExperiorSDK/fileenc.h>
+#import <UserExperiorSDK/hmac.h>
+#import <UserExperiorSDK/ioapi.h>
+#import <UserExperiorSDK/ioapi_buf.h>
+#import <UserExperiorSDK/ioapi_mem.h>
+#import <UserExperiorSDK/minishared.h>
+#import <UserExperiorSDK/prng.h>
+#import <UserExperiorSDK/pwd2key.h>
+#import <UserExperiorSDK/sha1.h>
+#import <UserExperiorSDK/unzip.h>
+#import <UserExperiorSDK/zip.h>
+
+#endif /* UEZipArchive_h */
